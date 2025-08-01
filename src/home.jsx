4c44498e1d3e47736d9cba70c465c01497ec822a
Carlos <carlos@singfit.com>
2025-08-01 11:07:08 -0700

    Sub Domain Updates2

--- src/home.jsx
+++ src/home.jsx
@@ -1,7 +1,7 @@
 // Home.jsx
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+
 
 export default function Home() {
   useEffect(() => {
